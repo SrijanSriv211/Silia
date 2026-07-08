9cc07331f435dbaf4fde7d608ab9f957d7301a78
SrijanSriv211 <QCoreNest@gmail.com>
2026-07-09 01:57:29 +0530

    take cmd args

--- prepare_data.py
+++ prepare_data.py
@@ -1,15 +1,12 @@
 from encoder import Encoder
 from rich.progress import track
 from colorama import Style, Fore, init
-import pandas as pd, pickle, json, os
+import pandas as pd, pickle, sys
 
 init(autoreset=True)
-
-dataset_path = [
-	"data/fineweb-edu-100M/train-00000-of-00002.parquet",
-	"data/fineweb-edu-100M/train-00001-of-00002.parquet"
-]
-enc_path = "bin/o12k.bin"
+dataset_path = sys.argv[1]
+enc_path = sys.argv[2]
+outpath = sys.argv[3]
 
 enc = Encoder()
 enc.load(enc_path)
@@ -21,11 +18,11 @@ data = df["text"].tolist()
 lsum = lambda x: sum([len(i) for i in x])
 
 n_chars = lsum(data)
-for i, x in enumerate(track(data, f"{Fore.WHITE}{Style.BRIGHT}encoding {Fore.WHITE}{Style.DIM}fineweb-100M{Style.RESET_ALL}")):
-	data[i] = enc.encode(f"{x}<|eot|>\n", allowed_special="all")
+for i, x in enumerate(track(data, f"{Fore.WHITE}{Style.BRIGHT}encoding {Fore.WHITE}{Style.DIM}dataset{Style.RESET_ALL}")):
+	data[i] = enc.encode(x, allowed_special="all")
 
 n_toks = lsum(data)
 print(f"{(n_chars/1e6)}M total chars,", f"{(n_toks/1e6)}M total tokens")
 
-with open("data/fineweb-100M.bin", "wb") as f:
+with open(outpath, "wb") as f:
 	pickle.dump({"dataset": data}, f)
